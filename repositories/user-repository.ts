17d121fbbb779add4d246c0ae96a1eb3de13ffb6
Edwin Shen <edwin@tripleaxle.com>
2024-06-08 22:57:19 +0900

    save

--- repositories/user-repository.ts
+++ repositories/user-repository.ts
@@ -7,33 +7,40 @@ export class UserRepository extends BaseRepository<Users> {
   protected tableName = 'users' as keyof DB
 
   async findByIdWithRoles(userId: string) {
-    const result = await db.selectFrom('users')
+    const result = await db
+      .selectFrom('users')
       .where('users.id', '=', userId)
-      .leftJoin('host_clubs', 'host_clubs.id', 'users.selected_host_club_id')
       .leftJoin('host_club_members', 'host_club_members.user_id', 'users.id')
+      .leftJoin('host_clubs', 'host_clubs.id', 'host_club_members.host_club_id')
       .leftJoin('roles', 'roles.id', 'host_club_members.role_id')
       .select([
-        sql<Users>`to_jsonb(users.*)`.as('users'),
-        sql<HostClubs | null>`CASE WHEN users.selected_host_club_id IS NULL THEN NULL ELSE to_jsonb(host_clubs.*) END`.as(
-          'host_club',
-        ),
-        sql<string | null>`
-          CASE
-            WHEN users.selected_host_club_id IS NULL THEN NULL
-            ELSE roles.name
-          END
-        `.as('role'),
+        sql<Selectable<Users>>`to_jsonb(users.*)`.as('user'),
+        sql<{
+          host_club: Selectable<HostClubs> | null
+          role: string | null
+        }[]>`jsonb_agg(jsonb_build_object(
+        'host_club', to_jsonb(host_clubs.*),
+        'role', roles.name
+      ))`.as('host_clubs_with_roles'),
       ])
+      .groupBy('users.id')
       .executeTakeFirst()
 
     if (!result)
       return null
 
+    const { user, host_clubs_with_roles } = result
+
+    const selectedClub
+    = host_clubs_with_roles.find(hc => hc.host_club?.id === user.selected_host_club_id)
+    || host_clubs_with_roles[0]
+    || null
+
     // create auth view and don't pass
     return {
-      ...result.users,
-      role: result.role,
-      host_club: result.host_club,
+      ...user,
+      role: selectedClub?.role ?? null,
+      host_club: selectedClub?.host_club ?? null,
     }
   }
 }
